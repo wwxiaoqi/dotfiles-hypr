292f6acca1adfdc631033cabf5e36b8df42eb991
wwxiaoqi <tahicoing@gmail.com>
2025-01-25 00:30:05 +0800

    fix: croc flag to CROC_SECRET="" croc

--- dot_config/fish/functions/croc.fish
+++ dot_config/fish/functions/croc.fish
@@ -1,3 +1,8 @@
 function croc
-    /usr/bin/croc --pass 's74Yv2DhmAtEEkRKTVFCKkJFfn' --relay '1.12.254.118:9009' $argv
+    if test (count $argv) -eq 1
+        set -x CROC_SECRET $argv[1]
+        /usr/bin/croc --pass 's74Yv2DhmAtEEkRKTVFCKkJFfn' --relay '1.12.254.118:9009'
+    else
+        /usr/bin/croc --pass 's74Yv2DhmAtEEkRKTVFCKkJFfn' --relay '1.12.254.118:9009' $argv
+    end
 end
