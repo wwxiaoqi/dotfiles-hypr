b00f8af3b6f22b9eaecbb93dbe2622467b2a2897
wwxiaoqi <tahicoing@gmail.com>
2025-01-28 20:47:55 +0800

    fix: daily updates

--- dot_config/fish/functions/fish_user_key_bindings.fish
+++ dot_config/fish/functions/fish_user_key_bindings.fish
@@ -1,4 +1,10 @@
 # Fish shell on vi mode
 function fish_user_key_bindings
+
+  # 修复 vi 模式下自动补全快捷键 control + f 不能用
+  for mode in insert default visual
+      bind -M $mode \cf forward-char
+  end
+
   fish_vi_key_bindings
 end
